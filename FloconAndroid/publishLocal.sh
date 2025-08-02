705668c15b067b5e9961edd8a07bd6534d96072c
Florent Champigny <florent@bere.al>
2025-08-02 13:13:19 +0200

    fixed publish file

--- FloconAndroid/publishLocal.sh
+++ FloconAndroid/publishLocal.sh
@@ -1,12 +1,10 @@
 ./gradlew \
   :core:assembleRelease \
   :grpc-interceptor:assembleRelease \
-  :okhttp-interceptor:assembleRelease \
-  :graphql-interceptor:assembleRelease
+  :okhttp-interceptor:assembleRelease
 
 ./gradlew \
   :core:publishToMavenLocal \
   :grpc-interceptor:publishToMavenLocal \
   :okhttp-interceptor:publishToMavenLocal \
-  :graphql-interceptor:publishToMavenLocal \
   -Psigning.required=false
