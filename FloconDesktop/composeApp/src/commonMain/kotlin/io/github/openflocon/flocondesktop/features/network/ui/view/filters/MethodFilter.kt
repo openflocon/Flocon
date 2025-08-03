94dcff86807f2c6b557320b34802e7ac74a1c061
Raphael Teyssandier <rteyssandier@sephora.fr>
2025-08-03 19:44:20 +0200

    featrure: Rework methods

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/network/ui/view/filters/MethodFilter.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/network/ui/view/filters/MethodFilter.kt
@@ -1,16 +1,14 @@
 package io.github.openflocon.flocondesktop.features.network.ui.view.filters
 
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.remember
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import flocondesktop.composeapp.generated.resources.Res
-import flocondesktop.composeapp.generated.resources.chevron_right
 import flocondesktop.composeapp.generated.resources.network
 import io.github.openflocon.flocondesktop.features.network.domain.model.FloconHttpRequestDomainModel
 import io.github.openflocon.flocondesktop.features.network.ui.view.components.FilterDropdown
@@ -22,8 +20,8 @@ import org.jetbrains.compose.resources.painterResource
 
 class MethodFilter : Filters {
 
-    private val selectedMethods = MutableStateFlow(emptyList<Method>())
-    
+    private val selectedMethods = MutableStateFlow(Methods.all())
+
     override val content: @Composable (() -> Unit) = {
         val methods by selectedMethods.collectAsStateWithLifecycle()
 
@@ -31,27 +29,28 @@ class MethodFilter : Filters {
             text = "Method",
             icon = painterResource(Res.drawable.network) // TODO Change
         ) {
-            Method.entries
+            Methods.all()
                 .forEach { method ->
                     val selected = methods.contains(method)
+                    val interactionSource = remember { MutableInteractionSource() }
+                    val onClick = {
+                        if (selected)
+                            remove(method)
+                        else
+                            add(method)
+                    }
 
                     DropdownMenuItem(
-                        text = { Text(text = method.text) },
+                        text = { Text(text = method.label) },
+                        interactionSource = interactionSource,
                         trailingIcon = {
-                            if (selected) {
-                                Icon(
-                                    painter = painterResource(Res.drawable.chevron_right), // TODO Change
-                                    contentDescription = null,
-                                    modifier = Modifier.size(20.dp)
-                                )
-                            }
+                            Checkbox(
+                                checked = selected,
+                                onCheckedChange = { onClick() },
+                                interactionSource = interactionSource
+                            )
                         },
-                        onClick = {
-                            if (selected)
-                                remove(method)
-                            else
-                                add(method)
-                        }
+                        onClick = onClick
                     )
                 }
         }
@@ -62,24 +61,56 @@ class MethodFilter : Filters {
             if (methods.isEmpty())
                 return@map list
 
-            list.filter { methods.map(Method::text).contains(it.request.method) }
+            list.filter { item ->
+                when (item.type) {
+                    is FloconHttpRequestDomainModel.Type.GraphQl -> methods.contains(Methods.GraphQL)
+                    is FloconHttpRequestDomainModel.Type.Grpc -> methods.contains(Methods.Grpc)
+                    is FloconHttpRequestDomainModel.Type.Http -> methods.filterIsInstance<Methods.Http>()
+                        .map(Methods.Http::query)
+                        .contains(item.request.method)
+                }
+            }
         }
     }
 
-    private fun add(method: Method) {
+    private fun add(method: Methods) {
         selectedMethods.update { it + method }
     }
 
-    private fun remove(method: Method) {
+    private fun remove(method: Methods) {
         selectedMethods.update { it - method }
     }
 
-    private enum class Method(val text: String) {
-        GET("GET"),
-        POST("POST"),
-        PUT("PUT"),
-        PATCH("PATCH"),
-        DELETE("DELETE")
+    sealed interface Methods {
+
+        enum class Http(
+            val query: String
+        ) : Methods {
+            GET("GET"),
+            POST("POST"),
+            PUT("PUT"),
+            PATCH("PATCH"),
+            DELETE("DELETE")
+        }
+
+        data object GraphQL : Methods
+
+        data object Grpc : Methods
+
+        companion object {
+            fun all(): List<Methods> = Http.entries + GraphQL + Grpc
+        }
     }
 
 }
+
+private val MethodFilter.Methods.label
+    get() = when (this) {
+        MethodFilter.Methods.GraphQL -> "GraphQL"
+        MethodFilter.Methods.Grpc -> "GRPC"
+        MethodFilter.Methods.Http.GET -> "Http - GET"
+        MethodFilter.Methods.Http.POST -> "Http - POST"
+        MethodFilter.Methods.Http.PUT -> "Http - PUT"
+        MethodFilter.Methods.Http.PATCH -> "Http - PATCH"
+        MethodFilter.Methods.Http.DELETE -> "Http - DELETE"
+    }
