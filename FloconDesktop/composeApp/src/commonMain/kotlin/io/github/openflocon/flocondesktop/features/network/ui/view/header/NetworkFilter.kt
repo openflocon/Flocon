167ac3b90cd54b12c52ea663ba822cd459e93c70
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-04 14:49:14 +0200

    fix: Icon

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/network/ui/view/header/NetworkFilter.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/network/ui/view/header/NetworkFilter.kt
@@ -1,28 +1,22 @@
 package io.github.openflocon.flocondesktop.features.network.ui.view.header
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import io.github.openflocon.flocondesktop.features.network.ui.NetworkAction
 import io.github.openflocon.flocondesktop.features.network.ui.NetworkUiState
 import io.github.openflocon.flocondesktop.features.network.ui.view.components.FilterBar
 import io.github.openflocon.flocondesktop.features.network.ui.view.filters.FilterMethods
+import io.github.openflocon.library.designsystem.components.FloconIconButton
 
 @Composable
 fun NetworkFilter(
@@ -41,18 +35,10 @@ fun NetworkFilter(
                 onTextChange = { onAction(NetworkAction.FilterQuery(it)) },
                 modifier = Modifier.weight(1f)
             )
-            Box(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(4.dp))
-                    .clickable(onClick = { onAction(NetworkAction.Reset) })
-                    .padding(all = 8.dp),
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.Delete,
-                    contentDescription = null,
-                    modifier = Modifier.size(20.dp),
-                )
-            }
+            FloconIconButton(
+                imageVector = Icons.Outlined.Delete,
+                onClick = { onAction(NetworkAction.Reset) }
+            )
         }
         Row(
             modifier = Modifier
