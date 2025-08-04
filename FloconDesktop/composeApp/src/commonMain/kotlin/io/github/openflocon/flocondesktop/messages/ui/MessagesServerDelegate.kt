59e4c193b7b73ce32869be02600dfd185810af9c
Florent Champigny <florent@bere.al>
2025-08-04 11:32:28 +0200

    fix: crash on start if port used

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/messages/ui/MessagesServerDelegate.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/messages/ui/MessagesServerDelegate.kt
@@ -1,7 +1,12 @@
 package io.github.openflocon.flocondesktop.messages.ui
 
+import io.github.openflocon.flocondesktop.SERVER_PORT
+import io.github.openflocon.flocondesktop.common.Either
+import io.github.openflocon.flocondesktop.common.Failure
+import io.github.openflocon.flocondesktop.common.Success
 import io.github.openflocon.flocondesktop.common.coroutines.closeable.CloseableDelegate
 import io.github.openflocon.flocondesktop.common.coroutines.closeable.CloseableScoped
+import io.github.openflocon.flocondesktop.common.ui.feedback.FeedbackDisplayer
 import io.github.openflocon.flocondesktop.messages.domain.HandleIncomingMessagesUseCase
 import io.github.openflocon.flocondesktop.messages.domain.StartServerUseCase
 import kotlinx.coroutines.flow.collect
@@ -11,9 +16,23 @@ class MessagesServerDelegate(
     private val startServerUseCase: StartServerUseCase,
     private val handleIncomingMessagesUseCase: HandleIncomingMessagesUseCase,
     private val closeableDelegate: CloseableDelegate,
+    private val feedbackDisplayer: FeedbackDisplayer,
 ) : CloseableScoped by closeableDelegate {
-    fun initialize() {
-        startServerUseCase()
+
+    fun initialize(): Either<Throwable, Unit> {
+        return try {
+            startServerUseCase()
+            Success(Unit)
+        } catch (t: Throwable) {
+            feedbackDisplayer.displayMessage(buildString {
+                append("Cannot start server on port $SERVER_PORT")
+                t.message?.let {
+                    append(" : ")
+                    append(it)
+                }
+            }, type = FeedbackDisplayer.MessageType.Error)
+            Failure(t)
+        }
 
         coroutineScope.launch {
             handleIncomingMessagesUseCase()
