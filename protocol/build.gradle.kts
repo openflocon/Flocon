bc0fb4420656459d16433018640aecbbe0f705b6
Florent Champigny <florent@bere.al>
2025-08-05 22:35:37 +0200

    upload protocol as dependency

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -6,6 +6,7 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.android.library)
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 kotlin {
@@ -46,6 +47,49 @@ android {
         compilerOptions {
             //jvmTarget.set(JvmTarget.JVM_11)
             languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        }
+    }
+}
+
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    if (project.hasProperty("signing.required") && project.property("signing.required") == "false") {
+        // Skip signing
+    } else {
+        signAllPublications()
+    }
+
+    coordinates(
+        groupId = project.property("floconGroupId") as String,
+        artifactId = "flocon-protocol",
+        version = System.getenv("PROJECT_VERSION_NAME") ?: project.property("floconVersion") as String
+    )
+
+    pom {
+        name = "Flocon Protocol"
+        description = project.property("floconDescription") as String
+        inceptionYear = "2025"
+        url = "https://github.com/openflocon/Flocon"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "openflocon"
+                name = "Open Flocon"
+                url = "https://github.com/openflocon"
+            }
+        }
+        scm {
+            url = "https://github.com/openflocon/Flocon"
+            connection = "scm:git:git://github.com/openflocon/Flocon.git"
+            developerConnection = "scm:git:ssh://git@github.com/openflocon/Flocon.git"
         }
     }
 }
--- scripts/publishToMavenLocal.sh
+++ scripts/publishToMavenLocal.sh
@@ -1,9 +1,11 @@
 ./gradlew \
+  :protocol:assembleRelease \
   :client:core:assembleRelease \
   :client:grpc-interceptor:assembleRelease \
   :client:okhttp-interceptor:assembleRelease
 
 ./gradlew \
+  :protocol:publishToMavenLocal \
   :client:core:publishToMavenLocal \
   :client:grpc-interceptor:publishToMavenLocal \
   :client:okhttp-interceptor:publishToMavenLocal \
