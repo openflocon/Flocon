4a047541268a4764bc689c7b856041ebba44583a
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-07 14:41:48 +0200

    fix: Upgrade db

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/common/db/AppDatabase.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/common/db/AppDatabase.kt
@@ -30,7 +30,7 @@ import io.github.openflocon.flocondesktop.features.table.data.datasource.local.m
 import kotlinx.coroutines.Dispatchers
 
 @Database(
-    version = 28,
+    version = 29,
     entities = [
         FloconHttpRequestEntity::class,
         FileEntity::class,
