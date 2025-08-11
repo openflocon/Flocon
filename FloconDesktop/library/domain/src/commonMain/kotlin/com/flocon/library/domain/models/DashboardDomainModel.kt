7288c4eae28047a2a5a37b09f388ec6e5d7cc233
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-11 14:20:14 +0200

    feature: Remove

--- FloconDesktop/library/domain/src/commonMain/kotlin/com/flocon/library/domain/models/DashboardDomainModel.kt
+++
@@ -1,6 +0,0 @@
-package io.github.openflocon.domain.models
-
-data class DashboardDomainModel(
-    val dashboardId: DashboardId,
-    val sections: List<DashboardSectionDomainModel>,
-)
--- FloconDesktop/library/domain/src/commonMain/kotlin/com/flocon/library/domain/models/NetworkTextFilterColumns.kt
+++
@@ -1,9 +0,0 @@
-package io.github.openflocon.domain.models
-
-enum class NetworkTextFilterColumns {
-    RequestTime,
-    Domain,
-    Query,
-    Status,
-    Time,
-}
