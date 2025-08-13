af545383bedbbe0bcdaa180ded7f5455bb1d32b3
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-13 22:46:15 +0200

    fix: Settings

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/app/ui/settings/SettingsScreen.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/app/ui/settings/SettingsScreen.kt
@@ -45,7 +45,6 @@ import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun SettingsScreen(
-    onCloseRequest: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val viewModel: SettingsViewModel = koinViewModel()
