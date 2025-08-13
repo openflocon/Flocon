48a893d0b854d8cd64c7f44a1237f3188a56648a
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-13 22:42:11 +0200

    feature: System notification

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/common/ui/feedback/FeedbackDisplayer.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/common/ui/feedback/FeedbackDisplayer.kt
@@ -1,9 +1,15 @@
 package io.github.openflocon.flocondesktop.common.ui.feedback
 
 interface FeedbackDisplayer {
+
     fun displayMessage(
         message: String,
         type: MessageType = MessageType.Success,
+    )
+
+    fun displayNotification(
+        title: String,
+        message: String
     )
 
     enum class MessageType {
--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/common/ui/feedback/FeedbackDisplayerHandler.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/common/ui/feedback/FeedbackDisplayerHandler.kt
@@ -4,10 +4,16 @@ import kotlinx.coroutines.flow.Flow
 
 interface FeedbackDisplayerHandler {
     val messagesToDisplay: Flow<MessageToDisplayUi>
+    val notificationsToDisplay: Flow<NotificationToDisplayUi>
 
     data class MessageToDisplayUi(
         val message: String,
         val type: FeedbackDisplayer.MessageType,
         val id: String,
+    )
+
+    data class NotificationToDisplayUi(
+        val title: String,
+        val message: String,
     )
 }
