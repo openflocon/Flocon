6bea453acf87de1586dedbdafcb3833103795493
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-13 21:38:19 +0200

    fix: DI

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/database/DI.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/database/DI.kt
@@ -1,8 +1,11 @@
 package io.github.openflocon.flocondesktop.features.database
 
+import io.github.openflocon.flocondesktop.features.database.delegate.DatabaseSelectorDelegate
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 internal val databaseModule = module {
     viewModelOf(::DatabaseViewModel)
+    singleOf(::DatabaseSelectorDelegate)
 }
