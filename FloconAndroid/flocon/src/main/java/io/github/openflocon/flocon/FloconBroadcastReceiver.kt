2afbf7cabe21993fc2a1e49b2a03919d01da86ce
Florent Champigny <florent@bere.al>
2025-08-14 18:18:37 +0200

    feat: [ADB] device serial detection

--- FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/FloconBroadcastReceiver.kt
+++ FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/FloconBroadcastReceiver.kt
@@ -9,5 +9,14 @@ class FloconBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         // Log le message pour l'afficher dans Logcat
         Log.d("FloconBroadcastReceiver", "Réception de la diffusion avec l'action: ${intent?.action}")
+
+        val serial = intent?.getStringExtra("serial")
+        if (serial != null) {
+            Log.d("FloconBroadcastReceiver", "serial : $serial")
+        }
     }
 }
+
+/*
+adb shell am broadcast -a "io.github.openflocon.flocon.DETECT" -n "io.github.openflocon.flocon.myapplication/io.github.openflocon.flocon.FloconBroadcastReceiver" --es "serial" "v859mbnzin5px8dq"
+ */
