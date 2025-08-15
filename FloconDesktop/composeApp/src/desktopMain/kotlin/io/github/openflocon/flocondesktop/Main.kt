0edfdedcefa23fa331e0de51799d4e1e0c2b2f28
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-15 15:50:13 +0200

    fix: Build

--- FloconDesktop/composeApp/src/desktopMain/kotlin/io/github/openflocon/flocondesktop/Main.kt
+++ FloconDesktop/composeApp/src/desktopMain/kotlin/io/github/openflocon/flocondesktop/Main.kt
@@ -1,9 +1,16 @@
 package io.github.openflocon.flocondesktop
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.ApplicationScope
+import androidx.compose.ui.window.FrameWindowScope
+import androidx.compose.ui.window.MenuBar
+import androidx.compose.ui.window.Notification
+import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberTrayState
@@ -14,6 +21,8 @@ import coil3.network.ktor3.KtorNetworkFetcherFactory
 import flocondesktop.composeapp.generated.resources.Res
 import flocondesktop.composeapp.generated.resources.app_icon_small
 import io.github.openflocon.flocondesktop.about.AboutScreen
+import io.github.openflocon.flocondesktop.common.ui.feedback.FeedbackDisplayer
+import io.github.openflocon.flocondesktop.common.ui.feedback.FeedbackDisplayerHandler
 import io.github.openflocon.flocondesktop.window.MIN_WINDOW_HEIGHT
 import io.github.openflocon.flocondesktop.window.MIN_WINDOW_WIDTH
 import io.github.openflocon.flocondesktop.window.WindowStateData
@@ -21,6 +30,7 @@ import io.github.openflocon.flocondesktop.window.WindowStateSaver
 import io.github.openflocon.flocondesktop.window.size
 import io.github.openflocon.flocondesktop.window.windowPosition
 import org.jetbrains.compose.resources.painterResource
+import org.koin.compose.koinInject
 import java.awt.Desktop
 import java.awt.Dimension
 
@@ -29,6 +39,11 @@ fun main() {
 
     return application {
         var openAbout by remember { mutableStateOf(false) }
+        val savedState = remember { WindowStateSaver.load() }
+        val windowState = rememberWindowState(
+            size = savedState.size(),
+            position = savedState.windowPosition(),
+        )
 
         Desktop.getDesktop().setAboutHandler {
             openAbout = true
@@ -42,25 +57,6 @@ fun main() {
                 }.build()
         }
 
-        LaunchedEffect(Unit) {
-            feedbackDisplayerHandler.notificationsToDisplay
-                .collect { notification ->
-                    trayState.sendNotification(
-                        Notification(
-                            title = notification.title,
-                            message = notification.message,
-                            type = when (notification.type) {
-                                FeedbackDisplayer.NotificationType.None -> Notification.Type.None
-                                FeedbackDisplayer.NotificationType.Info -> Notification.Type.Info
-                                FeedbackDisplayer.NotificationType.Warning -> Notification.Type.Warning
-                                FeedbackDisplayer.NotificationType.Error -> Notification.Type.Error
-                            }
-                        )
-                    )
-                }
-        }
-
-        FloconTray(trayState)
         Window(
             state = windowState,
             onCloseRequest = {
@@ -81,11 +77,10 @@ fun main() {
             icon = painterResource(Res.drawable.app_icon_small), // Remove black behind icon
         ) {
             window.minimumSize = Dimension(MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT)
+            App()
+            FloconTray()
             // TODO later
 //            FloconMenu()
-            App()
-
-            App()
 
             if (openAbout) {
                 AboutScreen(
@@ -122,7 +117,28 @@ private fun FrameWindowScope.FloconMenu() {
 }
 
 @Composable
-private fun ApplicationScope.FloconTray(trayState: TrayState) {
+private fun ApplicationScope.FloconTray() {
+    val trayState = rememberTrayState()
+    val feedbackDisplayerHandler = koinInject<FeedbackDisplayerHandler>()
+
+    LaunchedEffect(Unit) {
+        feedbackDisplayerHandler.notificationsToDisplay
+            .collect { notification ->
+                trayState.sendNotification(
+                    Notification(
+                        title = notification.title,
+                        message = notification.message,
+                        type = when (notification.type) {
+                            FeedbackDisplayer.NotificationType.None -> Notification.Type.None
+                            FeedbackDisplayer.NotificationType.Info -> Notification.Type.Info
+                            FeedbackDisplayer.NotificationType.Warning -> Notification.Type.Warning
+                            FeedbackDisplayer.NotificationType.Error -> Notification.Type.Error
+                        }
+                    )
+                )
+            }
+    }
+
     Tray(
         state = trayState,
         icon = painterResource(Res.drawable.app_icon_small)
