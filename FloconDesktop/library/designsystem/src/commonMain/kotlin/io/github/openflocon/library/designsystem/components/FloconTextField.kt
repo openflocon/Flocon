a0a3096be167573330bcdc95653fd8bad9fbf9c3
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-18 15:14:47 +0200

    fix: Cursor

--- FloconDesktop/library/designsystem/src/commonMain/kotlin/io/github/openflocon/library/designsystem/components/FloconTextField.kt
+++ FloconDesktop/library/designsystem/src/commonMain/kotlin/io/github/openflocon/library/designsystem/components/FloconTextField.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
@@ -54,6 +55,7 @@ fun FloconTextField(
         value = value,
         onValueChange = onValueChange,
         textStyle = textStyle,
+        cursorBrush = SolidColor(Color.White), // TODO Light mod
         decorationBox = {
             TextFieldDefaults.DecorationBox(
                 value = value,
