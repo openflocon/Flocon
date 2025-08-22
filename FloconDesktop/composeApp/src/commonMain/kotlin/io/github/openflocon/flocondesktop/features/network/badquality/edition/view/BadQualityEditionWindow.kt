adc6bddc179867b4c69481d9732df8350b3cc5f5
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-08-22 10:45:40 +0200

    fix: Use Flocon dialog

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/network/badquality/edition/view/BadQualityEditionWindow.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/network/badquality/edition/view/BadQualityEditionWindow.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -167,7 +166,7 @@ fun BadNetworkQualityEditionContent(
             },
         )
         selectedErrorToEdit?.let { selectedError ->
-            BasicAlertDialog(
+            FloconDialog(
                 onDismissRequest = {
                     selectedErrorToEdit = null
                 },
