685270d1f00f4177deacc526f6013e10e1bf7ec1
Florent Champigny <florent@bere.al>
2025-08-24 11:28:48 +0200

    refact: [GRPC] formatter can be overriden

--- FloconAndroid/app/src/main/java/io/github/openflocon/flocon/myapplication/grpc/InitializeGrpc.kt
+++ FloconAndroid/app/src/main/java/io/github/openflocon/flocon/myapplication/grpc/InitializeGrpc.kt
@@ -1,6 +1,5 @@
 package io.github.openflocon.flocon.myapplication.grpc
 
-import io.github.openflocon.flocon.grpc.lite.FloconGrpcFormatter
 import io.github.openflocon.flocon.grpc.lite.FloconGrpcInterceptor
 import io.grpc.CallOptions
 import io.grpc.ManagedChannel
