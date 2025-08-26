e338e21b9bd9e9506a1ac4c0ce359fa66b91f661
Florent Champigny <florent@bere.al>
2025-08-26 11:05:37 +0200

    screenshot works

--- FloconDesktop/domain/src/commonMain/kotlin/io/github/openflocon/domain/device/usecase/TakeScreenshotUseCase.kt
+++ FloconDesktop/domain/src/commonMain/kotlin/io/github/openflocon/domain/device/usecase/TakeScreenshotUseCase.kt
@@ -4,23 +4,59 @@ import io.github.openflocon.domain.adb.ExecuteAdbCommandUseCase
 import io.github.openflocon.domain.adb.model.AdbCommandTargetDomainModel
 import io.github.openflocon.domain.common.Either
 import io.github.openflocon.domain.common.Failure
+import io.github.openflocon.domain.common.then
+import java.io.File
+import java.nio.file.Paths
 
 class TakeScreenshotUseCase(
     private val getCurrentDeviceIdAndPackageNameUseCase: GetCurrentDeviceIdAndPackageNameUseCase,
     private val executeAdbCommandUseCase: ExecuteAdbCommandUseCase,
 ) {
+
     suspend operator fun invoke() : Either<Throwable, Unit> {
         val current = getCurrentDeviceIdAndPackageNameUseCase() ?: return Failure(Throwable("No device selected"))
 
-        val userHome = System.getProperty("user.home")
+        val fileName = "screenshot_${current.packageName}_${System.currentTimeMillis()}.png"
+        val onDeviceFilePath = "/sdcard/$fileName"
+        val desktopPath = Paths.get(System.getProperty("user.home"), "Desktop", fileName)
 
-        // TODO windows adb exec-out screencap -p > C:\Users\VotreNomUtilisateur\Desktop\capture.png
         return executeAdbCommandUseCase(
             target = AdbCommandTargetDomainModel.Device(current.deviceId),
-            command = "exec-out screencap -p > $userHome/Desktop/test.png", //${current.packageName}_${System.currentTimeMillis()}.png",
-        ).mapSuccess {
-            println(it)
-        }
+            command = "shell screencap -p $onDeviceFilePath",
+        ).then {
+            executeAdbCommandUseCase(
+                target = AdbCommandTargetDomainModel.Device(current.deviceId),
+                command = "pull $onDeviceFilePath $desktopPath",
+            )
+        }.then {
+            executeAdbCommandUseCase(
+                target = AdbCommandTargetDomainModel.Device(current.deviceId),
+                command = "shell rm $onDeviceFilePath",
+            )
+        }.mapSuccess {  }
             .alsoFailure { it.printStackTrace() }
+    }
+}
+
+// TODO move to a repo
+
+private fun getDesktopPath(): String {
+    val userHome = System.getProperty("user.home")
+    return "$userHome${File.separator}Desktop"
+}
+
+private fun createDesktopFile(fileName: String) : File? {
+    val desktopPath = getDesktopPath()
+    val file = File(desktopPath, fileName)
+
+    try {
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        println("Fichier créé avec succès sur le bureau : ${file.absolutePath}")
+        return file
+    } catch (e: Exception) {
+        println("Erreur lors de la création du fichier : ${e.message}")
+        return null
     }
 }
