af8d005b69b3a5f4ae18a23ee0f414aa039dc7cf
Florent Champigny <florent@bere.al>
2025-08-31 08:59:37 +0200

    refact: [DESKTOP] remove android target

--- FloconDesktop/composeApp/src/androidMain/kotlin/io/github/openflocon/flocondesktop/common/utils/Link.android.kt
+++
@@ -1,7 +0,0 @@
-package io.github.openflocon.flocondesktop.common.utils
-
-import java.net.URI
-
-actual fun openInBrowser(uri: URI) {
-    // no op
-}
--- FloconDesktop/library/designsystem/src/androidMain/kotlin/io/github/openflocon/library/designsystem/common/ClipboardManager.android.kt
+++
@@ -1,3 +0,0 @@
-package io.github.openflocon.library.designsystem.common
-
-actual fun copyToClipboard(text: String) = Unit
