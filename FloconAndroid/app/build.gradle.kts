9ac1b30a3c744f95d9111bfc57966fc0923ac655
Florent Champigny <florent@bere.al>
2025-08-31 11:01:06 +0200

    min sdk 21

--- FloconAndroid/app/build.gradle.kts
+++ FloconAndroid/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.openflocon.flocon.myapplication"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
