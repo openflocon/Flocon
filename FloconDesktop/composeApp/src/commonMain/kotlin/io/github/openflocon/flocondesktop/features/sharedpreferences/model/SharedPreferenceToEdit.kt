74c3858d9bbf492f1041414f3a9c2f63dc959188
Florent Champigny <florent@bere.al>
2025-09-09 16:39:40 +0200

    refact

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/sharedpreferences/model/SharedPreferenceToEdit.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/sharedpreferences/model/SharedPreferenceToEdit.kt
@@ -3,7 +3,7 @@ package io.github.openflocon.flocondesktop.features.sharedpreferences.model
 import androidx.compose.runtime.Immutable
 
 @Immutable
-internal data class SharedPreferenceToEdit(
+data class SharedPreferenceToEdit(
     val row: SharedPreferencesRowUiModel,
     val stringValue: SharedPreferencesRowUiModel.Value.StringValue,
 )
