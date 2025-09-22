3881c8731941f2617fbaadafe5c7fea0dfd5ddb7
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-09-22 15:55:47 +0200

    feature: Improve UI & Add permission

--- FloconDesktop/library/designsystem/src/commonMain/kotlin/io/github/openflocon/library/designsystem/components/FloconTab.kt
+++ FloconDesktop/library/designsystem/src/commonMain/kotlin/io/github/openflocon/library/designsystem/components/FloconTab.kt
@@ -2,10 +2,12 @@ package io.github.openflocon.library.designsystem.components
 
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -13,12 +15,16 @@ fun FloconTab(
     selected: Boolean,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    selectedContentColor: Color = LocalContentColor.current,
+    unselectedContentColor: Color = selectedContentColor,
     content: @Composable ColumnScope.() -> Unit
 ) {
     Tab(
         selected = selected,
         onClick = onClick,
         content = content,
+        selectedContentColor = selectedContentColor,
+        unselectedContentColor = unselectedContentColor,
         modifier = modifier
     )
 }
@@ -28,11 +34,15 @@ fun FloconTab(
     text: String,
     selected: Boolean,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    selectedContentColor: Color = LocalContentColor.current,
+    unselectedContentColor: Color = selectedContentColor,
 ) {
     FloconTab(
         selected = selected,
         onClick = onClick,
+        selectedContentColor = selectedContentColor,
+        unselectedContentColor = unselectedContentColor,
         modifier = modifier
     ) {
         Text(
