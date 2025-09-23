4f6b0aa07ebee1e53a95764018db4cdd2b4bc1a8
Florent Champigny <florent@bere.al>
2025-09-23 16:57:53 +0200

    fix

--- FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/Flocon.kt
+++ FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/Flocon.kt
@@ -2,18 +2,11 @@ package io.github.openflocon.flocon
 
 import android.content.Context
 import io.github.openflocon.flocon.client.FloconClientImpl
-import io.github.openflocon.flocon.core.FloconMessageSender
-import io.github.openflocon.flocon.plugins.analytics.FloconAnalyticsPlugin
-import io.github.openflocon.flocon.plugins.dashboard.FloconDashboardPlugin
-import io.github.openflocon.flocon.plugins.deeplinks.FloconDeeplinksPlugin
-import io.github.openflocon.flocon.plugins.device.startActivityTracker
-import io.github.openflocon.flocon.plugins.tables.FloconTablePlugin
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
