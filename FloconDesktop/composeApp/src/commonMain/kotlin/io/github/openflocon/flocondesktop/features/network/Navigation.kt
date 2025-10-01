6911a89cbf3455192b4c19f007ec1cd3112a3815
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-10-01 13:35:13 +0200

    feat: Add draft of menu

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/network/Navigation.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/network/Navigation.kt
@@ -3,13 +3,19 @@ package io.github.openflocon.flocondesktop.features.network
 import androidx.navigation3.runtime.EntryProviderBuilder
 import io.github.openflocon.flocondesktop.features.network.list.view.NetworkScreen
 import io.github.openflocon.navigation.FloconRoute
+import io.github.openflocon.navigation.scene.Menu
+import io.github.openflocon.navigation.scene.MenuScene
 import kotlinx.serialization.Serializable
 
 @Serializable
 data object NetworkRoute : FloconRoute
 
 fun EntryProviderBuilder<FloconRoute>.networkNavigation() {
-    entry<NetworkRoute> {
+    entry<NetworkRoute>(
+        metadata = mapOf(
+            MenuScene.MENU_KEY to Menu.HOME // TODO Change
+        )
+    ) {
         NetworkScreen()
     }
 }
