81af88aa000c479e3a5839d37c6e3fe544578fce
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-10-01 13:48:51 +0200

    feat: Move menu

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/app/AppViewModel.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/app/AppViewModel.kt
@@ -5,8 +5,16 @@ import androidx.lifecycle.viewModelScope
 import io.github.openflocon.domain.common.DispatcherProvider
 import io.github.openflocon.domain.settings.usecase.InitAdbPathUseCase
 import io.github.openflocon.domain.settings.usecase.StartAdbForwardUseCase
+import io.github.openflocon.flocondesktop.main.ui.buildLeftPanelState
+import io.github.openflocon.flocondesktop.main.ui.model.SubScreen
+import io.github.openflocon.flocondesktop.main.ui.model.id
 import io.github.openflocon.flocondesktop.messages.ui.MessagesServerDelegate
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
@@ -19,6 +27,19 @@ class AppViewModel(
 ) : ViewModel(
     messagesServerDelegate,
 ) {
+
+    val subScreen = MutableStateFlow(SubScreen.Network)
+    val leftPanelState = subScreen.map { subScreen ->
+        buildLeftPanelState(
+            selectedId = subScreen.id,
+        )
+    }
+        .flowOn(dispatcherProvider.ui)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Eagerly,
+            initialValue = buildLeftPanelState(subScreen.value.id),
+        )
 
     init {
         viewModelScope.launch(dispatcherProvider.viewModel) {
