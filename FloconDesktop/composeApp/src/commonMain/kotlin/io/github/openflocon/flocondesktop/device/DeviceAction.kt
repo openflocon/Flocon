efcdcebce8d6624e38a791176254b1601adad1fd
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-10-01 10:27:06 +0200

    feat: Rework tabs

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/device/DeviceAction.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/device/DeviceAction.kt
@@ -2,7 +2,7 @@ package io.github.openflocon.flocondesktop.device
 
 internal sealed interface DeviceAction {
 
-    data class SelectTab(val index: Int) : DeviceAction
+    data class SelectTab(val selected: DeviceTab) : DeviceAction
 
     data object Refresh : DeviceAction
 
