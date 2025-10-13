a112e0a9244e3af353bb5435879a32834fde6910
Florent Champigny <florent@bere.al>
2025-10-13 09:57:28 +0200

    adjust colors

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/database/view/DatabaseQueryView.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/database/view/DatabaseQueryView.kt
@@ -2,18 +2,13 @@ package io.github.openflocon.flocondesktop.features.database.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
@@ -24,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.graphicsLayer
@@ -50,7 +44,27 @@ import io.github.openflocon.library.designsystem.components.FloconButton
 import io.github.openflocon.library.designsystem.components.FloconTextField
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+interface SqlColorPalette {
+    val keywordColor: Color
+    val stringColor: Color
+    val numberColor: Color
+    val functionColor: Color
+    val textColor: Color
+}
+
+object DefaultSqlPalette : SqlColorPalette {
+    override val keywordColor = Color(0xFFfcaf28)
+    override val stringColor = Color(0xFF4CAF50)
+    override val numberColor = Color(0xFF03A9F4)
+    override val functionColor = Color(0xFF82AAFF)
+    override val textColor = Color(0xFFD0D0D0)
+}
+
+
+
 class ColorsTransformation() : VisualTransformation {
+
+    val colorPalette : SqlColorPalette = DefaultSqlPalette
 
     private fun buildAnnotatedStringWithColors(text: AnnotatedString): AnnotatedString {
         return buildAnnotatedString {
@@ -67,18 +81,22 @@ class ColorsTransformation() : VisualTransformation {
                 val upper = token.uppercase()
                 when {
                     sqlKeywords.contains(upper.trim()) -> withStyle(
-                        SpanStyle(color = Color(0xFF4FC3F7), fontWeight = FontWeight.Bold)
+                        SpanStyle(color = colorPalette.keywordColor, fontWeight = FontWeight.Bold)
                     ) { append(token) }
 
                     token.startsWith("'") && token.endsWith("'") -> withStyle(
-                        SpanStyle(color = Color(0xFF81C784))
+                        SpanStyle(color = colorPalette.stringColor)
                     ) { append(token) }
 
                     token.matches(Regex("\\d+")) -> withStyle(
-                        SpanStyle(color = Color(0xFFFFB74D))
+                        SpanStyle(color = colorPalette.numberColor)
                     ) { append(token) }
 
-                    else -> append(token)
+                    else -> {
+                        withStyle(
+                            SpanStyle(color = colorPalette.textColor)
+                        ) { append(token) }
+                    }
                 }
             }
         }
