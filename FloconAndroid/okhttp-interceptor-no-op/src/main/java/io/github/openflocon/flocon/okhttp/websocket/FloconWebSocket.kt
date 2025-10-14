98819568012095fe5bf64aaa47dad278c876e226
Florent Champigny <florent@bere.al>
2025-10-14 23:34:00 +0200

    fix: no op websocket

--- FloconAndroid/okhttp-interceptor-no-op/src/main/java/io/github/openflocon/flocon/okhttp/websocket/FloconWebSocket.kt
+++ FloconAndroid/okhttp-interceptor-no-op/src/main/java/io/github/openflocon/flocon/okhttp/websocket/FloconWebSocket.kt
@@ -8,6 +8,6 @@ import okio.ByteString
 fun WebSocket.sendWithFlocon(text: String) = this.send(text) // simple method call, adds no flocon override
 fun WebSocket.sendWithFlocon(bytes: ByteString) = this.send(bytes) // simple method call, adds no flocon override
 
-fun WebSocketListener.listenWithFlocon(): WebSocketListener {
+fun WebSocketListener.listenWithFlocon(id: String): WebSocketListener {
     return this
 }
