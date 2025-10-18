8314db3f1b2e2837bc2a7f1557465079d3f524fc
Florent Champigny <florent@bere.al>
2025-10-18 14:50:48 +0200

    removed json inject

--- FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/plugins/files/model/fromdevice/FilesResultDataModel.kt
+++ FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/plugins/files/model/fromdevice/FilesResultDataModel.kt
@@ -1,20 +1,15 @@
 package io.github.openflocon.flocon.plugins.files.model.fromdevice
 
-import org.json.JSONArray
-import org.json.JSONObject
+import io.github.openflocon.flocon.core.FloconEncoder
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
 
+@Serializable
 internal data class FilesResultDataModel(
     val requestId: String,
     val files: List<FileDataModel>,
 ) {
-    fun toJson(): JSONObject {
-        val json = JSONObject()
-
-        json.put("requestId", requestId)
-        json.put("files", JSONArray(
-            files.map { it.toJson() })
-        )
-
-        return json
+    fun toJson(): String {
+        return FloconEncoder.json.encodeToString(this)
     }
 }
--- FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/plugins/sharedprefs/model/todevice/ToDeviceGetSharedPrefsMessage.kt
+++ FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/plugins/sharedprefs/model/todevice/ToDeviceGetSharedPrefsMessage.kt
@@ -1,22 +1,18 @@
 package io.github.openflocon.flocon.plugins.sharedprefs.model.todevice
 
 import io.github.openflocon.flocon.FloconLogger
-import org.json.JSONObject
+import io.github.openflocon.flocon.core.FloconEncoder
+import kotlinx.serialization.Serializable
 
+@Serializable
 internal data class ToDeviceGetSharedPrefsMessage(
     val requestId: String,
 ) {
     companion object {
         fun fromJson(message: String): ToDeviceGetSharedPrefsMessage? {
             return try {
-                val jsonObject = JSONObject(message)
-
-                val requestId = jsonObject.getString("requestId")
-
-                ToDeviceGetSharedPrefsMessage(
-                    requestId = requestId,
-                )
-            }  catch (t: Throwable) {
+                FloconEncoder.json.decodeFromString(message)
+            } catch (t: Throwable) {
                 FloconLogger.logError("parsing issue", t)
                 null
             }
