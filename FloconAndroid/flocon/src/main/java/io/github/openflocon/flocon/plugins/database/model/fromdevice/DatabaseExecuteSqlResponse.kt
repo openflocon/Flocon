1dcb1bf0c9fe4383ae1857379311e7a7352fcd1d
Florent Champigny <florent@bere.al>
2025-10-18 15:54:10 +0200

    fixed database

--- FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/plugins/database/model/fromdevice/DatabaseExecuteSqlResponse.kt
+++ FloconAndroid/flocon/src/main/java/io/github/openflocon/flocon/plugins/database/model/fromdevice/DatabaseExecuteSqlResponse.kt
@@ -1,89 +1,60 @@
 package io.github.openflocon.flocon.plugins.database.model.fromdevice
 
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.add
-import kotlinx.serialization.json.addJsonArray
+import io.github.openflocon.flocon.core.FloconEncoder
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.encodeToJsonElement
 import kotlinx.serialization.json.put
-import kotlinx.serialization.json.putJsonArray
-import kotlinx.serialization.json.putJsonObject
 
+@Serializable
 internal sealed interface DatabaseExecuteSqlResponse {
 
+    @Serializable
     // Case for successful SELECT queries
-    data class Select(
+    class Select(
         val columns: List<String>,
         val values: List<List<String?>>
     ) : DatabaseExecuteSqlResponse
 
     // Case for successful INSERT queries
-    data class Insert(
+    @Serializable
+    class Insert(
         val insertedId: Long
     ) : DatabaseExecuteSqlResponse
 
     // Case for successful UPDATE or DELETE queries
-    data class UpdateDelete(
+    @Serializable
+    class UpdateDelete(
         val affectedCount: Int
     ) : DatabaseExecuteSqlResponse
 
     // Case for successful "raw" queries (CREATE TABLE, DROP TABLE, etc.)
+    @Serializable
     object RawSuccess : DatabaseExecuteSqlResponse
 
     // Case for an SQL execution error
-    data class Error(
+    @Serializable
+    class Error(
         val message: String,       // Detailed error message
         val originalSql: String,   // SQL query that caused the error (optional)
     ) : DatabaseExecuteSqlResponse
 }
 
-internal fun DatabaseExecuteSqlResponse.toJson(): JsonObject {
-    return buildJsonObject {
-        when (this@toJson) {
-            is DatabaseExecuteSqlResponse.Error -> {
-                put("type", "Error")
-                putJsonObject("body") {
-                    put("message", message)
-                    put("originalSql", originalSql)
-                }
-            }
+internal fun DatabaseExecuteSqlResponse.toJson(): String {
+    val jsonEncoder = FloconEncoder.json
+    val thisAsJson = jsonEncoder.encodeToJsonElement(this)
 
-            is DatabaseExecuteSqlResponse.Insert -> {
-                put("type", "Insert")
-                putJsonObject("body") {
-                    put("insertedId", insertedId)
-                }
-            }
-
-            DatabaseExecuteSqlResponse.RawSuccess -> {
-                put("type", "RawSuccess")
-                putJsonObject("body") {
-                    // empty
-                }
-            }
-
-            is DatabaseExecuteSqlResponse.Select -> {
-                put("type", "Select")
-                putJsonObject("body") {
-                    putJsonArray("columns") {
-                        columns.forEach { add(it) }
-                    }
-                    putJsonArray("values") {
-                        values.forEach { row ->
-                            addJsonArray {
-                                row.forEach { item -> add(item) }
-                            }
-                        }
-                    }
-                }
-            }
-
-            is DatabaseExecuteSqlResponse.UpdateDelete -> {
-                put("type", "UpdateDelete")
-                putJsonObject("body") {
-                    put("affectedCount", affectedCount)
-                }
-            }
-        }
+    val type = when (this) {
+        is DatabaseExecuteSqlResponse.Error -> "Error"
+        is DatabaseExecuteSqlResponse.Insert -> "Insert"
+        DatabaseExecuteSqlResponse.RawSuccess -> "RawSuccess"
+        is DatabaseExecuteSqlResponse.Select -> "Select"
+        is DatabaseExecuteSqlResponse.UpdateDelete -> "UpdateDelete"
     }
+
+    return buildJsonObject {
+        put("type", type)
+        put("body", thisAsJson.toString())
+    }.toString()
 }
 
