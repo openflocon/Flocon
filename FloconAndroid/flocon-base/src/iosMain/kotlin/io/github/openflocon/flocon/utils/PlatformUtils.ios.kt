b572c490d06232d70ee43ed5797908c9b6d323a3
Florent Champigny <florent@bere.al>
2025-10-20 14:06:46 +0200

    refact: [GRADLE] ktor moved to kmp

--- FloconAndroid/flocon-base/src/iosMain/kotlin/io/github/openflocon/flocon/utils/PlatformUtils.ios.kt
+++ FloconAndroid/flocon-base/src/iosMain/kotlin/io/github/openflocon/flocon/utils/PlatformUtils.ios.kt
@@ -2,9 +2,14 @@ package io.github.openflocon.flocon.utils
 
 import platform.Foundation.NSDate
 import platform.Foundation.timeIntervalSince1970
+import kotlin.time.TimeSource
 
 actual fun currentTimeMillis(): Long {
     return (NSDate().timeIntervalSince1970 * 1000).toLong()
+}
+
+actual fun currentTimeNanos(): Long {
+    return TimeSource.Monotonic.markNow().elapsedNow().inWholeNanoseconds
 }
 
 actual fun createThrowableFromClassName(className: String): Throwable? {
