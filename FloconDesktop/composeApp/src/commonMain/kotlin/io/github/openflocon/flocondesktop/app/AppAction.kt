467a09bfafcc2e306c0dfdb296ffafe2398a7574
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-10-29 19:56:32 +0100

    fix: Topbar

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/app/AppAction.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/app/AppAction.kt
@@ -1,9 +1,25 @@
 package io.github.openflocon.flocondesktop.app
 
+import io.github.openflocon.flocondesktop.app.ui.model.DeviceAppUiModel
+import io.github.openflocon.flocondesktop.app.ui.model.DeviceItemUiModel
 import io.github.openflocon.flocondesktop.app.ui.model.SubScreen
 
 internal sealed interface AppAction {
 
     data class SelectMenu(val menu: SubScreen) : AppAction
+
+    data class DeleteApp(val app: DeviceAppUiModel) : AppAction
+
+    data class DeleteDevice(val device: DeviceItemUiModel) : AppAction
+
+    data class SelectApp(val app: DeviceAppUiModel) : AppAction
+
+    data class SelectDevice(val device: DeviceItemUiModel) : AppAction
+
+    data object Record : AppAction
+
+    data object Restart : AppAction
+
+    data object Screenshoot : AppAction
 
 }
