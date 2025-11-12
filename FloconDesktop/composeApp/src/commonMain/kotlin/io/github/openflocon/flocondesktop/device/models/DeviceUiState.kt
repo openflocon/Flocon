26829d7ce7c4aeb56d80228893891822c9cd2643
TEYSSANDIER Raphael <rteyssandier@sephora.fr>
2025-11-12 13:34:45 +0100

    feature: Info

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/device/models/DeviceUiState.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/device/models/DeviceUiState.kt
@@ -6,13 +6,11 @@ import androidx.compose.runtime.Immutable
 data class DeviceUiState(
     val deviceSerial: String,
     val contentState: ContentUiState,
-    val infoState: InfoUiState,
     val memoryState: MemoryUiState
 )
 
 internal fun previewDeviceUiState() = DeviceUiState(
     deviceSerial = "",
     contentState = previewContentUiState(),
-    memoryState = previewMemoryUiState(),
-    infoState = previewInfoUiState()
+    memoryState = previewMemoryUiState()
 )
