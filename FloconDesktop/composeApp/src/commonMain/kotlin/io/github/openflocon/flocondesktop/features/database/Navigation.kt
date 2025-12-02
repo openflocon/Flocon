80a78bc69cf0ed355e4715811572ef98d41aba19
Raphael TEYSSANDIER <raphael.teyssandier@gmail.com>
2025-12-02 15:53:08 +0100

    fix: Panel db

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/database/Navigation.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/database/Navigation.kt
@@ -2,14 +2,24 @@ package io.github.openflocon.flocondesktop.features.database
 
 import androidx.navigation3.runtime.EntryProviderScope
 import io.github.openflocon.flocondesktop.app.MenuSceneStrategy
+import io.github.openflocon.flocondesktop.features.database.view.DatabaseDetails
 import io.github.openflocon.flocondesktop.features.database.view.DatabaseScreen
+import io.github.openflocon.flocondesktop.features.database.view.DetailResultItem
 import io.github.openflocon.navigation.FloconRoute
+import io.github.openflocon.navigation.PanelRoute
+import io.github.openflocon.navigation.scene.PanelSceneStrategy
 import kotlinx.serialization.Serializable
 
 sealed interface DatabaseRoutes : FloconRoute {
 
     @Serializable
     data object Main : DatabaseRoutes
+
+    @Serializable
+    data class Detail(
+        val result: DetailResultItem,
+        val columns: List<String>
+    ) : DatabaseRoutes, PanelRoute
 }
 
 fun EntryProviderScope<FloconRoute>.databaseRoutes() {
@@ -17,5 +27,16 @@ fun EntryProviderScope<FloconRoute>.databaseRoutes() {
         metadata = MenuSceneStrategy.menu()
     ) {
         DatabaseScreen()
+    }
+    entry<DatabaseRoutes.Detail>(
+        metadata = PanelSceneStrategy.panel(
+            pinnable = false,
+            closable = true
+        )
+    ) {
+        DatabaseDetails(
+            state = it.result,
+            columns = it.columns
+        )
     }
 }
