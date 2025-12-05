bd2f12b7a1d29bd638ad9425b861e10f850a78b1
Florent Champigny <florent@bere.al>
2025-12-05 16:04:08 +0100

    fixed kmp sample

--- FloconAndroid/sample-multiplatform/src/commonMain/kotlin/io/github/openflocon/flocon/myapplication/multi/dashboard/InitializeDashboard.kt
+++ FloconAndroid/sample-multiplatform/src/commonMain/kotlin/io/github/openflocon/flocon/myapplication/multi/dashboard/InitializeDashboard.kt
@@ -8,16 +8,13 @@ import io.github.openflocon.flocon.myapplication.multi.dashboard.user.User
 import io.github.openflocon.flocon.myapplication.multi.dashboard.user.userFlow
 import io.github.openflocon.flocon.plugins.dashboard.dsl.button
 import io.github.openflocon.flocon.plugins.dashboard.dsl.checkBox
-import io.github.openflocon.flocon.plugins.dashboard.dsl.form
 import io.github.openflocon.flocon.plugins.dashboard.dsl.json
 import io.github.openflocon.flocon.plugins.dashboard.dsl.label
 import io.github.openflocon.flocon.plugins.dashboard.dsl.plainText
-import io.github.openflocon.flocon.plugins.dashboard.dsl.section
 import io.github.openflocon.flocon.plugins.dashboard.dsl.text
 import io.github.openflocon.flocon.plugins.dashboard.dsl.textField
 import io.github.openflocon.flocon.plugins.dashboard.floconDashboard
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -29,64 +26,51 @@ data class DashboardData(
 
 fun initializeDashboard() {
     GlobalScope.launch {
-        combine(
-            userFlow,
-            tokensFlow,
-        ) { user, tokens ->
-            DashboardData(
-                user = user,
-                tokens = tokens,
-            )
-        }.collect { (user, tokens) ->
-            floconDashboard(id = "main") {
-                user?.let {
-                    section(name = "User") {
-                        text(label = "username", value = user.userName)
-                        text(label = "fullName", value = user.fullName, color = Color.Red.toArgb())
-                        text(label = "user id", value = user.id)
-                        label(label = "actions :")
-                        button(
-                            text = "Change User Id",
-                            id = "changeUserId",
-                            onClick = {
-                                userFlow.update { it.copy(userName = "__flo__") }
-                            }
-                        )
-                        textField(
-                            label = "Update Name",
-                            placeHolder = "name",
-                            id = "changeUserName",
-                            value = user.fullName,
-                            onSubmitted = { value ->
-                                userFlow.update { it.copy(fullName = value) }
-                            })
+        floconDashboard(id = "main") {
+            section(name = "User", userFlow) { user ->
+                text(label = "username", value = user.userName)
+                text(label = "fullName", value = user.fullName, color = Color.Red.toArgb())
+                text(label = "user id", value = user.id)
+                label(label = "actions :")
+                button(
+                    text = "Change User Id",
+                    id = "changeUserId",
+                    onClick = {
+                        userFlow.update { it.copy(userName = "__flo__") }
                     }
-                }
-                tokens?.let {
-                    section(name = "Tokens") {
-                        text(label = "accessToken", value = tokens.accessToken)
-                        text(label = "refreshToken", value = tokens.refreshToken)
-                        text(label = "expiration", value = tokens.expiration)
-                        button(
-                            text = "Clear Access Token",
-                            id = "clearAccessToken",
-                            onClick = {
-                                println("cleaned access token")
-                            }
-                        )
+                )
+                textField(
+                    label = "Update Name",
+                    placeHolder = "name",
+                    id = "changeUserName",
+                    value = user.fullName,
+                    onSubmitted = { value ->
+                        userFlow.update { it.copy(fullName = value) }
+                    })
+            }
+            section(name = "Tokens", tokensFlow) { tokens ->
+                text(label = "accessToken", value = tokens.accessToken)
+                text(label = "refreshToken", value = tokens.refreshToken)
+                text(label = "expiration", value = tokens.expiration)
+                button(
+                    text = "Clear Access Token",
+                    id = "clearAccessToken",
+                    onClick = {
+                        println("cleaned access token")
                     }
-                }
+                )
             }
         }
     }
-    floconDashboard(id = "plainText") {
-        section(name = "Test") {
-            plainText(
-                label = "lorem",
-                value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ullamcorper elit. Pellentesque turpis ex, cursus cursus urna sed, iaculis sagittis nisl. Curabitur vehicula nunc eu metus rhoncus placerat. Vivamus at placerat ligula. Morbi ullamcorper cursus tellus, vitae molestie lorem sollicitudin euismod. Sed ullamcorper, risus vitae facilisis tempor, elit leo accumsan purus, ut ultricies augue erat et justo. Duis efficitur mauris eu finibus tincidunt. Aenean magna libero, auctor quis turpis et, viverra porta lorem. Ut tempus odio sit amet vestibulum condimentum. Donec et augue quis arcu blandit sodales. In laoreet odio id turpis ultricies, eu ornare dui blandit. Morbi hendrerit velit turpis, eget ornare ex consequat id. Nullam rhoncus, libero et sollicitudin tristique, risus ipsum luctus neque, ultricies ullamcorper felis metus non turpis. Nullam sed accumsan sem, at fermentum tortor."
-            )
-            json(
-                label = "json", value = """
+    GlobalScope.launch {
+        floconDashboard(id = "plainText") {
+            section(name = "Test") {
+                plainText(
+                    label = "lorem",
+                    value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ullamcorper elit. Pellentesque turpis ex, cursus cursus urna sed, iaculis sagittis nisl. Curabitur vehicula nunc eu metus rhoncus placerat. Vivamus at placerat ligula. Morbi ullamcorper cursus tellus, vitae molestie lorem sollicitudin euismod. Sed ullamcorper, risus vitae facilisis tempor, elit leo accumsan purus, ut ultricies augue erat et justo. Duis efficitur mauris eu finibus tincidunt. Aenean magna libero, auctor quis turpis et, viverra porta lorem. Ut tempus odio sit amet vestibulum condimentum. Donec et augue quis arcu blandit sodales. In laoreet odio id turpis ultricies, eu ornare dui blandit. Morbi hendrerit velit turpis, eget ornare ex consequat id. Nullam rhoncus, libero et sollicitudin tristique, risus ipsum luctus neque, ultricies ullamcorper felis metus non turpis. Nullam sed accumsan sem, at fermentum tortor."
+                )
+                json(
+                    label = "json", value = """
                                 {
                                   "testData": {
                                     "name": "John Doe",
@@ -112,33 +96,36 @@ fun initializeDashboard() {
                                   "message": "Test data loaded successfully."
                                 }
                     """.trimIndent()
-            )
+                )
+            }
         }
     }
 
-    floconDashboard(id = "form") {
-        form(
-            name = "Test form",
-            submitText = "Submit form test button",
-            onSubmitted = { values ->
-                values.forEach { (key, value) ->
-                    println("$key - $value")
-                    // input_1 - test
-                    // checkbox_1 - true
+    GlobalScope.launch {
+        floconDashboard(id = "form") {
+            form(
+                name = "Test form",
+                submitText = "Submit form test button",
+                onSubmitted = { values ->
+                    values.forEach { (key, value) ->
+                        println("$key - $value")
+                        // input_1 - test
+                        // checkbox_1 - true
+                    }
                 }
+            ) {
+                textField(
+                    id = "input_1",
+                    label = "Input field 1",
+                    placeHolder = "placeholder",
+                    value = "test"
+                )
+                checkBox(
+                    id = "checkbox_1",
+                    label = "Checkbox 1",
+                    value = true,
+                )
             }
-        ) {
-            textField(
-                id = "input_1",
-                label = "Input field 1",
-                placeHolder = "placeholder",
-                value = "test"
-            )
-            checkBox(
-                id = "checkbox_1",
-                label = "Checkbox 1",
-                value = true,
-            )
         }
     }
 }
