ac273ae19dfe610d4282029097de39b6d511c4a6
Florent Champigny <florent@bere.al>
2026-01-12 10:51:46 +0100

    feat: [PERFORMANCES] add a performances monitor

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/performance/view/PerformanceDetailScreen.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/features/performance/view/PerformanceDetailScreen.kt
@@ -1,9 +1,20 @@
 package io.github.openflocon.flocondesktop.features.performance.view
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,7 +54,7 @@ fun PerformanceDetailScreen(
             Card(
                 modifier = Modifier.fillMaxWidth(),
                 colors = CardDefaults.cardColors(
-                    containerColor = FloconTheme.colorPalette.onSurface
+                    containerColor = FloconTheme.colorPalette.surface
                 )
             ) {
                 Column(
@@ -59,21 +70,13 @@ fun PerformanceDetailScreen(
 
             // Screenshot
             if (event.screenshotPath != null) {
-                Card(
-                    modifier = Modifier.fillMaxWidth().weight(1f, fill = false),
-                    colors = CardDefaults.cardColors(
-                        containerColor = FloconTheme.colorPalette.onSurface
-                    )
-                ) {
-                    AsyncImage(
-                        model = "file://${event.screenshotPath}",
-                        contentDescription = "Large Screenshot",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        contentScale = ContentScale.Fit
-                    )
-                }
+                AsyncImage(
+                    model = "file://${event.screenshotPath}",
+                    contentDescription = "Large Screenshot",
+                    modifier = Modifier.fillMaxWidth()
+                        .weight(1f, fill = false),
+                    contentScale = ContentScale.Fit
+                )
             } else {
                 Text(
                     "No screenshot available",
