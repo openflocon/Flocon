9e0064115c64e86bad7d431110f5699e879ed522
doTTTTT <raphael.teyssandier@gmail.com>
2026-03-09 21:15:54 +0100

    fix: Version

--- FloconAndroid/flocon-no-op/build.gradle.kts
+++ FloconAndroid/flocon-no-op/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
@@ -5,7 +7,11 @@ plugins {
 }
 
 kotlin {
-    androidTarget()
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
     
     jvm()
 
--- FloconAndroid/ktor-interceptor-no-op/build.gradle.kts
+++ FloconAndroid/ktor-interceptor-no-op/build.gradle.kts
@@ -7,7 +7,11 @@ plugins {
 }
 
 kotlin {
-    androidTarget()
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
     
     jvm()
 
