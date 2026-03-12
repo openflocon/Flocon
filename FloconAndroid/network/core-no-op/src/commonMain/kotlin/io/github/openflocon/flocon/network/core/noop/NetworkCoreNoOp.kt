57d1f1bcd0a7fc3e241c0b2963b3ea76c0a59362
doTTTTT <raphael.teyssandier@gmail.com>
2026-03-12 16:38:03 +0100

    feat: No op

--- FloconAndroid/network/core-no-op/src/commonMain/kotlin/io/github/openflocon/flocon/network/core/noop/NetworkCoreNoOp.kt
+++
@@ -1,3 +0,0 @@
-package io.github.openflocon.flocon.network.core.noop
-
-// Placeholder for Network Core No-Op
---
+++ FloconAndroid/network/core-no-op/src/commonMain/kotlin/io/github/openflocon/flocon/network/core/noop/plugin/FloconNetworkPluginImpl.kt
@@ -0,0 +1,32 @@
+package io.github.openflocon.flocon.network.core.noop.plugin
+
+import io.github.openflocon.flocon.FloconPlugin
+import io.github.openflocon.flocon.pluginsold.network.FloconNetworkPlugin
+import io.github.openflocon.flocon.pluginsold.network.model.BadQualityConfig
+import io.github.openflocon.flocon.pluginsold.network.model.FloconNetworkCallRequest
+import io.github.openflocon.flocon.pluginsold.network.model.FloconNetworkCallResponse
+import io.github.openflocon.flocon.pluginsold.network.model.FloconWebSocketEvent
+import io.github.openflocon.flocon.pluginsold.network.model.FloconWebSocketMockListener
+import io.github.openflocon.flocon.pluginsold.network.model.MockNetworkResponse
+
+internal class FloconNetworkPluginImpl : FloconPlugin, FloconNetworkPlugin {
+    override val key: String = "NETWORK"
+    override val mocks: Collection<MockNetworkResponse> = emptyList()
+    override val badQualityConfig: BadQualityConfig? = null
+
+    override suspend fun onMessageReceived(method: String, body: String) = Unit // No op
+    override suspend fun onConnectedToServer() = Unit // No op
+
+    override fun logRequest(request: FloconNetworkCallRequest) = Unit // No op
+    override fun logResponse(response: FloconNetworkCallResponse) = Unit // No op
+
+    override suspend fun logWebSocket(event: FloconWebSocketEvent) = Unit // No op
+    override suspend fun registerWebSocketMockListener(
+        id: String,
+        listener: FloconWebSocketMockListener
+    ) = Unit // No op
+
+    companion object {
+        var plugin: FloconNetworkPlugin? = null
+    }
+}
