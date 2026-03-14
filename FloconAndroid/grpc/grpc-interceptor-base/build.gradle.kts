30d860cde2b461790ae1ce11974a37c7106b859e
doTTTTT <raphael.teyssandier@gmail.com>
2026-03-14 23:58:37 +0100

    feat: Clean

--- FloconAndroid/grpc/grpc-interceptor-base/build.gradle.kts
+++ FloconAndroid/grpc/grpc-interceptor-base/build.gradle.kts
@@ -1,38 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    id("com.vanniktech.maven.publish") version "0.34.0"
+    id("flocon.android.library")
+    id("flocon.publish")
 }
+
 
 android {
     namespace = "io.github.openflocon.flocon.grpc.base"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 23
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
+
 
 dependencies {
     implementation(project(":flocon"))
@@ -45,44 +20,9 @@ dependencies {
 
 
 mavenPublishing {
-    publishToMavenCentral(automaticRelease = true)
-
-    if (project.hasProperty("signing.required") && project.property("signing.required") == "false") {
-        // Skip signing
-    } else {
-        signAllPublications()
-    }
-
     coordinates(
         groupId = project.property("floconGroupId") as String,
         artifactId = "flocon-grpc-interceptor-base",
         version = System.getenv("PROJECT_VERSION_NAME") ?: project.property("floconVersion") as String
     )
-
-
-    pom {
-        name = "Flocon Grpc Interceptor"
-        description = project.property("floconDescription") as String
-        inceptionYear = "2025"
-        url = "https://github.com/openflocon/Flocon"
-        licenses {
-            license {
-                name = "The Apache License, Version 2.0"
-                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-            }
-        }
-        developers {
-            developer {
-                id = "openflocon"
-                name = "Open Flocon"
-                url = "https://github.com/openflocon"
-            }
-        }
-        scm {
-            url = "https://github.com/openflocon/Flocon"
-            connection = "scm:git:git://github.com/openflocon/Flocon.git"
-            developerConnection = "scm:git:ssh://git@github.com/openflocon/Flocon.git"
-        }
-    }
-}
+}
