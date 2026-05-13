1d2399b4036a3182e6ebd9fb5fe9457cc0383899
Raphael TEYSSANDIER <raphael.teyssandier@gmail.com>
2026-05-13 14:51:54 +0200

    fix: Delete duplicate

--- FloconAndroid/flocon/src/commonMain/kotlin/io/github/openflocon/flocon/plugins/database/model/fromdevice/DeviceDataBaseDataModel.kt
+++
@@ -1,9 +0,0 @@
-package io.github.openflocon.flocon.plugins.database.model.fromdevice
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-internal data class DeviceDataBaseDataModel(
-    val id: String,
-    val name: String,
-)
--- FloconAndroid/flocon/src/commonMain/kotlin/io/github/openflocon/flocon/plugins/database/model/fromdevice/QueryResultReceivedDataModel.kt
+++
@@ -1,9 +0,0 @@
-package io.github.openflocon.flocon.plugins.database.model.fromdevice
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-internal data class QueryResultDataModel(
-    val requestId: String,
-    val result: String,
-)
