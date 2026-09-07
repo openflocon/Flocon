d5908401b726465934acb7f0f1c158d8c42a9911
Raphael TEYSSANDIER <raphael.teyssandier@gmail.com>
2026-09-07 13:06:21 +0200

    fix: Build

--- FloconAndroid/sample-multiplatform/src/desktopMain/kotlin/io/github/openflocon/flocon/myapplication/multi/Main.kt
+++ FloconAndroid/sample-multiplatform/src/desktopMain/kotlin/io/github/openflocon/flocon/myapplication/multi/Main.kt
@@ -1,8 +1,5 @@
-@file:OptIn(ExperimentalComposeUiApi::class)
-
 package io.github.openflocon.flocon.myapplication.multi
 
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.room.Room
