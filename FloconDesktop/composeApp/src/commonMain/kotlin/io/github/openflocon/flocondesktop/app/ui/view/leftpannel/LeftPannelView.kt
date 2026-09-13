729ae406431fe90a582c8f434371bc04149e7d29
Sebastian Neubauer <sebastian.neubauer@exaring.de>
2026-09-13 14:24:48 +0200

    Make left panel scrollable

--- FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/app/ui/view/leftpannel/LeftPannelView.kt
+++ FloconDesktop/composeApp/src/commonMain/kotlin/io/github/openflocon/flocondesktop/app/ui/view/leftpannel/LeftPannelView.kt
@@ -7,13 +7,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,6 +31,8 @@ import io.github.openflocon.flocondesktop.app.ui.model.leftpanel.MenuState
 import io.github.openflocon.flocondesktop.app.ui.model.leftpanel.previewMenuState
 import io.github.openflocon.flocondesktop.app.ui.view.displayName
 import io.github.openflocon.library.designsystem.FloconTheme
+import io.github.openflocon.library.designsystem.components.FloconVerticalScrollbar
+import io.github.openflocon.library.designsystem.components.rememberFloconScrollbarAdapter
 import org.jetbrains.compose.resources.stringResource
 
 val PanelMaxWidth = 275.dp
@@ -40,26 +47,44 @@ fun LeftPanelView(
     onClickItem: (MenuItem) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
+    val scrollState = rememberScrollState()
+    Box(
         modifier = modifier
             .clip(FloconTheme.shapes.medium)
             .background(FloconTheme.colorPalette.primary)
-            .padding(8.dp)
+            .padding(8.dp),
     ) {
-        MenuSection(
-            current = current,
-            items = state.sections,
-            expanded = expanded,
-            onClickItem = onClickItem,
-        )
-        Spacer(modifier = Modifier.height(12.dp))
-        Spacer(Modifier.weight(1f))
-        MenuItems(
-            current = current,
-            items = state.bottomItems,
-            expanded = expanded,
-            onClickItem = onClickItem,
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .verticalScroll(scrollState)
+        ) {
+            MenuSection(
+                current = current,
+                items = state.sections,
+                expanded = expanded,
+                onClickItem = onClickItem,
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Spacer(Modifier.weight(1f))
+            MenuItems(
+                current = current,
+                items = state.bottomItems,
+                expanded = expanded,
+                onClickItem = onClickItem,
+            )
+        }
+
+        if(expanded) {
+            Spacer(modifier = Modifier.width(16.dp))
+
+            FloconVerticalScrollbar(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .align(Alignment.TopEnd),
+                adapter = rememberFloconScrollbarAdapter(scrollState),
+            )
+        }
     }
 }
 
